9b138e87174eca174d5b863100fe0c23d1ee357f
Thaycod171 <nunesthayane057@gmail>
2025-07-25 15:10:08 -0300

    atualização das pastas

---
+++ Primeiros passos com Python/Desafio-002/exercicio-01.py
@@ -0,0 +1,11 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas 
+
+nome = input("Digite seu nome: ")
+#print("Bem vindo!", nome)
+
+
+#podemos formatar o nome da pessoa utilizando o f de format
+
+print(f'Bem vindo! {nome}')
+
+#precisamos colocar o nome do usuário sempre dentro das chaves quando for formata-lo, desse jeito fica mais fácil de se chamar uma váriavel no print
